6ca93ad72538ef7bddb88c6b40cb904c11dd456c
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2019-04-14 10:06:22 +0300

    Initial commit. Create DI and Service Provider

---
+++ engine/Cms.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace Engine;
+
+class Cms {
+  private $di;
+
+  public function __construct($di) {
+    $this->di = $di;  
+  } 
+
+  public function run() {
+    print_r($this->di);
+  }
+}
---
+++ engine/bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Engine\Cms;
+use Engine\DI\DI;
+
+try {
+  $di = new DI(); 
+
+  $services = require __DIR__ . '/Config/Service.php';
+
+  foreach ($services as $service) {
+    $provider = new $service($di); 
+    $provider->init();
+  }
+
+  $cms = new Cms($di);
+  $cms->run();
+} catch (\ErrorException $e) {
+  echo $e->getMessage();
+}
