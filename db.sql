991e921627bec98852bbaed86e6353372e4efb16
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2019-04-21 14:21:20 +0300

    Update DB

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 20, 2019 at 10:18 PM
+-- Generation Time: Apr 21, 2019 at 11:20 AM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -38,8 +38,32 @@ INSERT INTO `page` (`id`, `title`, `content`, `date`) VALUES
 (4, 'dfdf', '<p>df​df</p>', '2019-04-20 21:56:47'),
 (5, 'some', '<p>some new​</p>', '2019-04-20 21:58:16'),
 (6, 'first', '<p>first​</p>', '2019-04-20 21:58:48'),
-(7, 'docker', '<p>docker content​</p>', '2019-04-20 22:04:28'),
-(8, 'd', '<p>d​</p>', '2019-04-20 22:10:19');
+(7, 'dockertt', '<p>docker content​</p>', '2019-04-20 22:04:28'),
+(8, 'd', '<p>d​</p>', '2019-04-20 22:10:19'),
+(9, 'New', '<p>​new page</p>', '2019-04-21 10:11:22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `setting`
+--
+
+CREATE TABLE `setting` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `key_field` varchar(100) NOT NULL,
+  `value` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `setting`
+--
+
+INSERT INTO `setting` (`id`, `name`, `key_field`, `value`) VALUES
+(1, 'Name site', 'name_site', 'Cms'),
+(2, 'Description', 'description', 'Example Description'),
+(3, 'Admin email', 'admin_email', 'admin@admin.com'),
+(4, 'Language', 'language', 'english');
 
 -- --------------------------------------------------------
 
@@ -61,7 +85,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `email`, `password`, `role`, `hash`, `date_reg`) VALUES
-(1, 'admin@admin.com', 'b59c67bf196a4758191e42f76670ceba', 'admin', 'e6d844f9d8dad0e6266ad944e3af9342', '2019-04-16 18:16:33'),
+(1, 'admin@admin.com', 'b59c67bf196a4758191e42f76670ceba', 'admin', 'f8e50250e00be3840ba9b180a0da1ac8', '2019-04-16 18:16:33'),
 (2, 'adminNew@admin.com', 'b59c67bf196a4758191e42f76670ceba', 'user', 'new', '2019-04-20 14:28:25'),
 (3, 'adminNew@admin.com', 'b59c67bf196a4758191e42f76670ceba', 'user', 'new', '2019-04-20 14:28:25'),
 (4, 'adminNew@admin.com', 'b59c67bf196a4758191e42f76670ceba', 'user', 'new', '2019-04-20 14:28:26'),
@@ -81,6 +105,14 @@ ALTER TABLE `page`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `setting`
+--
+ALTER TABLE `setting`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`),
+  ADD UNIQUE KEY `key` (`key_field`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -94,7 +126,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `page`
 --
 ALTER TABLE `page`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `setting`
+--
+ALTER TABLE `setting`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user`
